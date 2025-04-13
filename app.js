b84b2183f40fc5fa6bfa273bacfcd82511d4449f
unknown <garvishjain1997@gmail.com>
2025-04-13 21:09:36 +0530

    local statergy integration

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ require('./database/model')
 require('dotenv').config();
 const http = require('http');
 const passport = require('passport');
+var LocalStrategy = require('passport-local');
 const routes = require('./routes/routes')
 const expressSession = require('express-session');
+const {users} = require('./database/model');
 require('./apps/passportconfig')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(expressSession({secret:'secret',resave:false,saveUninitialized:false}))
+
+/** Passport Middleware */
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -35,6 +39,62 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     next()
 })
+
+/** Create Local Strategies */
+passport.use(new LocalStrategy(
+  async function(username, password, done) {
+    console.log(username, password,users);
+    try {
+      const userData = await users.findOne({ where: { email: username } });
+      // console.log(userData,"userData");
+      if (!userData) { return done(null, false); }
+      if (!userData.password == password) { return done(null, false); }
+      // console.log(userData);
+      
+      return done(null, userData);
+    } catch (error) {
+      console.log(error); 
+    }
+  }
+));
+
+app.post('/register', 
+  async (req,res,done)=>{
+    const userData = await users.findOne({ where: { email: req.body.username } });
+    // if(userData == "" || userData == null) done(null, false)
+    if(userData) res.json({msg:"User Is Already Exist."})
+    else {
+      let userData = await users.create({email:req.body.username,password: req.body.password,first_name:req.body.firstname,last_name:req.body.lastname,mobileno:req.body.mobileno})
+      console.log(userData,"userData");
+      res.json({msg: "user is created.",result: userData})
+    }
+  },
+  passport.authenticate('local'),
+  // function(req, res) {
+  //   res.json({msg:`${userData.email} user is created`})
+  //   // res.json(req.user)
+  //   // res.redirect('/');
+  // }
+);
+
+app.post('/login', 
+  passport.authenticate('local'),
+  function(req, res) {
+    res.json(req.user)
+    // res.redirect('/');
+  });
+
+/** Serialize & Deserialize User */
+passport.serializeUser((userData, done) => {
+  if(userData){
+    done(null, userData.id)
+  }
+  done(null, false)
+});
+passport.deserializeUser(async(id, done) => {
+  const user = await users.findByPk(id);
+  done(null, user);
+});
 
 
 // app.use(user)
--- routes/routes.js
+++ routes/routes.js
@@ -20,12 +20,12 @@ Routes.route('/getUser').post(jwt_auth.authenticate,UserController.getUser)
 /** Passport Routes */
 Routes.route('/').get((req, res) => res.send('Home Page'));
 
-Routes.route('/login').get((req, res) => res.send('<form action="/api/login" method="POST"><input name="username"/><input name="password" type="password"/><button type="submit">Login</button></form>'));
+// Routes.route('/login').get((req, res) => res.send('<form action="/api/login" method="POST"><input name="username"/><input name="password" type="password"/><button type="submit">Login</button></form>'));
 
-Routes.route('/login').post(passport.authenticate('local', {
-  successRedirect: '/api/dashboard',
-  failureRedirect: '/api/login'
-}));
+// Routes.route('/login').post(passport.authenticate('local', {
+//   successRedirect: '/api/dashboard',
+//   failureRedirect: '/api/login'
+// }));
 
 Routes.route('/dashboard').get(isAuthenticated, (req, res) => res.send(`Welcome ${req.user.first_name}! <a href="/api/logout">Logout</a>`));
 
